4df9fd24922fcf993ae984ab2f7a1a05d4ae5406
KaterinaKn24 <katerina2409@ukr.net>
2024-01-08 18:48:07 +0100

    six

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,13 +12,29 @@
 const button = document.getElementById('btn');
 const sunn = document.querySelector('.sun');
 
-const colors = [ "white", "yellow" ];
+const lft = document.getElementById('left');
+const rght = document.getElementById('right');
+
+const colors = [ "white", "rgb(206, 206, 48)" ];
 
 button.addEventListener('click', () => {
-sunn.style.backgroundColor = colors[getRandomNumber()];
+sunn.style.backgroundColor = colors [getRandomNumber()];
 });
 
+lft.addEventListener('click', () => {
+    sunn.classList.add('left');
+});
 
 function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);
-}
+};
+
+//function setScrollBy() {
+    //window.scrollBy(20, 0);
+    //const windowScrollLeft = window.scrollX;
+    //console.log(windowScrollLeft);
+//};
+
+//lft.addEventListener('click', () => {
+    //sunn.windowcrollLeft =  window.windowScrollLeft.setScrollBy();
+//});
